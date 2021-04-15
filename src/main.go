3b4a14dc812e871e647d14b094165f28fb257bbd
Norberto Hideaki Enomoto <norberto.enomoto@gmail.com>
2021-04-15 20:40:54 -0300

    First commit

---
+++ src/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+var counter int
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	name, err := os.Hostname()
+
+	if err != nil {
+		panic(err)
+	}
+
+	counter++
+	fmt.Fprintf(w, "Hello Golang: "+name+" -> "+fmt.Sprint(counter))
+	fmt.Println("Hello Golang: " + name + " -> " + fmt.Sprint(counter))
+}
+
+func handleRequests() {
+	http.HandleFunc("/hello-golang", hello)
+	log.Fatal(http.ListenAndServe(":9002", nil))
+}
+
+func main() {
+	handleRequests()
+}
